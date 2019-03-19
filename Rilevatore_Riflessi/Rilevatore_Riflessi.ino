db572595a5625fef5e705c2f7384c44e5e81b39e
Niccolò <Torresan>
2019-03-19 18:42:03 +0100

    Primo riflesso perfezionato e secondo riflesso finito 19/03/2019

--- Rilevatore_Riflessi/Rilevatore_Riflessi.ino
+++ Rilevatore_Riflessi/Rilevatore_Riflessi.ino
@@ -5,7 +5,10 @@ int btn_Inizio = 1;
 int btn_PrimoRiflesso = 3;
 int btn_SecondoRiflesso = 5;
 int ledBlu = 7;
-int buzzer = 13;
+int ledRosso = 9;
+int ledVerde = 11;
+int buzzer = 12;
+
 int tempoAccensioneLed = 0;
 int tempoBuzzer = 0;
 
@@ -17,16 +20,39 @@ void setup() {
   pinMode(btn_SecondoRiflesso, INPUT);
   pinMode(ledBlu, OUTPUT);
   pinMode(buzzer, OUTPUT);
+  pinMode(ledRosso, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
 }
  
-void loop() {
- // lcd.setCursor(0,0);
- // lcd.print("");
- 
+void loop() {   
+    
+  lcd.setCursor(0,0);
+
+  tempoAccensioneLed = 0;
+  tempoBuzzer = 0;
+  
   if (digitalRead(btn_Inizio) == HIGH)
   {
+    lcd.clear();
+    digitalWrite(ledBlu, LOW);
+    digitalWrite(ledRosso, LOW);
+    digitalWrite(ledVerde, LOW);
+    
+    lcd.print("Inizia il test!");
+    delay(3000);
     primoRiflesso();
     secondoRiflesso();
+
+    if (tempoAccensioneLed < 500 && tempoBuzzer < 500)
+    {
+      lcd.println("Hai superato il Test!");
+      digitalWrite(ledVerde, HIGH);
+    }
+    else
+    {
+      lcd.println("Non hai superato il Test, ritenta!");
+      digitalWrite(ledRosso, HIGH);
+    }
   }
   else
   {
@@ -36,6 +62,7 @@ void loop() {
 
 void primoRiflesso()
 {
+  lcd.clear();
   delay(random(2000, 8000));
   digitalWrite(ledBlu, HIGH);
 
@@ -45,6 +72,7 @@ void primoRiflesso()
     delay(1);    //il tempo mi aumenta di un millisecondo ogni volta 
   }
 
+  lcd.print(tempoAccensioneLed + " ms");
   digitalWrite(ledBlu, LOW);
 }
 
@@ -58,6 +86,7 @@ void secondoRiflesso()
     tempoBuzzer++;
     delay(1);
   }
-
+  
+  lcd.println(tempoBuzzer + " ms");
   digitalWrite(buzzer, LOW);
 }
