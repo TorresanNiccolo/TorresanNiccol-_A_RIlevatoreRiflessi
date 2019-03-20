6c636cecaffabd0f216f03d472b9d0e1cbe9a273
Niccolò <Torresan>
2019-03-20 15:49:39 +0100

    Rilevatore di riflessi completato 20/03/2019

--- Rilevatore_Riflessi/Rilevatore_Riflessi.ino
+++ Rilevatore_Riflessi/Rilevatore_Riflessi.ino
@@ -1,20 +1,27 @@
 #include <LiquidCrystal_I2C.h>                
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-int btn_Inizio = 1;
-int btn_PrimoRiflesso = 3;
+//VARIABILI IN INPUT OVVERO I 3 BOTTONI PRESENTI
+int btn_Inizio          = 1;
+int btn_PrimoRiflesso   = 3;
 int btn_SecondoRiflesso = 5;
-int ledBlu = 7;
-int ledRosso = 9;
-int ledVerde = 11;
-int buzzer = 12;
 
+//VARIABILI IN OUTPIUT, OVVERO I LED BLU, ROSSO, VERDE E IL BUZZER
+int ledBlu              = 7;
+int ledRosso            = 9;
+int ledVerde            = 11;
+int buzzer              = 12;
+
+//VARIABILI PER I TEMPI SULLE QUALI SI STABILIRA' IL RISULTATO
 int tempoAccensioneLed = 0;
 int tempoBuzzer = 0;
+String s = " ms"; //STRINGA CREATA PER EVITARE ERRORI POI NEL DISPLAY
 
 void setup() {
   lcd.init();
   lcd.backlight();
+  
+  //STABLISCO QUALE SEGNALE E' IN ENTRATA E QUALE E' IN USCITA
   pinMode(btn_Inizio, INPUT);
   pinMode(btn_PrimoRiflesso, INPUT);
   pinMode(btn_SecondoRiflesso, INPUT);
@@ -25,68 +32,71 @@ void setup() {
 }
  
 void loop() {   
-    
   lcd.setCursor(0,0);
-
-  tempoAccensioneLed = 0;
-  tempoBuzzer = 0;
   
   if (digitalRead(btn_Inizio) == HIGH)
-  {
-    lcd.clear();
-    digitalWrite(ledBlu, LOW);
-    digitalWrite(ledRosso, LOW);
+  {    
     digitalWrite(ledVerde, LOW);
+    digitalWrite(ledRosso, LOW);
     
-    lcd.print("Inizia il test!");
-    delay(3000);
+    lcd.print("Inizia il Test!");
+    delay(2000);
+    lcd.clear();    
+      
+    tempoAccensioneLed = 0;
+    tempoBuzzer = 0;
+    
     primoRiflesso();
     secondoRiflesso();
 
+    //IF DI CONTROLLO CHE VERIFICA SE L'UTENTE HA SUPERATO O MENO IL TEST
     if (tempoAccensioneLed < 500 && tempoBuzzer < 500)
     {
-      lcd.println("Hai superato il Test!");
+      lcd.clear();
+      lcd.print("Test superato!");
       digitalWrite(ledVerde, HIGH);
     }
     else
     {
-      lcd.println("Non hai superato il Test, ritenta!");
-      digitalWrite(ledRosso, HIGH);
+      lcd.clear();
+      lcd.print("Ritenta!");
+      digitalWrite(ledRosso, HIGH);     
     }
   }
   else
   {
-    
   }
 }
 
+//CICLO PER IL PRIMO RIFLESSO (LED BLU)
 void primoRiflesso()
 {
-  lcd.clear();
-  delay(random(2000, 8000));
+  delay(random(2000, 8000));                              //GENERO UN NUMERO RANDOM IN MILLISECONDI DOVE IN QUEL PRECISO TEMPO IL LED SI ACCENDERA'
   digitalWrite(ledBlu, HIGH);
 
-  while (digitalRead(btn_PrimoRiflesso) != HIGH)
+  while (digitalRead(btn_PrimoRiflesso) != HIGH)          //CICLO DOVE IL TEMPO AUMENTA FINO A CHE L'UTENTE NON PREME IL SECONDO BOTTONE
   {
     tempoAccensioneLed++;
-    delay(1);    //il tempo mi aumenta di un millisecondo ogni volta 
+    delay(1);                                            //IL TEMPO AUMENTA DI UN MILLISECONDO OGNI CICLO 
   }
 
-  lcd.print(tempoAccensioneLed + " ms");
+  lcd.print(tempoAccensioneLed + s);                     //SCRIVO SUL DISPLAY IL CONTENUTO DELLA VARIABIL"tempoAccensioneLed"
   digitalWrite(ledBlu, LOW);
 }
 
+//CICLO PER IL SECONDO RIFLESSO (BUZZER)
 void secondoRiflesso()
 {
-  delay(random(2000, 8000));
+  delay(random(2000, 8000));                             //GENERO UN NUMERO RANDOM IN MILLISECONDI DOVE IN QUEL PRECISO TEMPO IL LED SI ACCENDERA'
   digitalWrite(buzzer, HIGH);
 
-  while (digitalRead(btn_SecondoRiflesso) != HIGH)
+  while (digitalRead(btn_SecondoRiflesso) != HIGH)       //CICLO DOVE IL TEMPO AUMENTA FINO A CHE L'UTENTE NON PREME IL TERZO BOTTONE
   {
     tempoBuzzer++;
-    delay(1);
+    delay(1);                                            //IL TEMPO AUMENTA DI UN MILLISECONDO OGNI CICLO
   }
-  
-  lcd.println(tempoBuzzer + " ms");
   digitalWrite(buzzer, LOW);
+  lcd.setCursor(0, 1);
+  lcd.print(tempoBuzzer + s);                            //SCRIVO SUL DISPLAY IL CONTENUTO DELLA VARIABILE "tempoBuzzer"
+  delay(2000);
 }
